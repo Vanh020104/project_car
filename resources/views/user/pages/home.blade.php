c7a8a7b131a23d7fb92b017691b13ba4418dd430
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2023-10-07 12:50:46 +0700

    Update home.blade.php

--- resources/views/user/pages/home.blade.php
+++ resources/views/user/pages/home.blade.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="zxx">
-
-<head>
-    <title>Home-exam_php</title>
-
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Rentaly - Multipurpose Vehicle Car Rental Website Template" name="description">
-    <meta content="" name="keywords">
-    <meta content="" name="author">
-    <!-- CSS Files
-    ================================================== -->
-    @include("user.layouts.head")
-</head>
-
-<body onload="initialize()">
-<div id="wrapper">
-
-    <!-- page preloader begin -->
-    <div id="de-preloader"></div>
-    <!-- page preloader close -->
-
-    <!-- header begin -->
-    @include("user.layouts.hearder")
-    <!-- header close -->
-    <!-- content begin -->
+@extends("user.layouts.app")
+@section("content")
     <div class="no-bottom no-top" id="content">
         <div id="top"></div>
         <section id="section-hero" aria-label="section" class="jarallax">
@@ -730,25 +705,4 @@
         </section>
 
     </div>
-    <!-- content close -->
-    <a href="#" id="back-to-top"></a>
-    <!-- footer begin -->
-    @include("user.layouts.footer")
-    <!-- footer close -->
-</div>
-<div id="selector">
-    <div id="demo-rtl" class="sc-opt">
-        <div class="sc-icon">RTL</div><span class="sc-val">Click to Enable</span>
-    </div>
-</div>
-<div id="purchase-now">
-
-    <div class="pn-hover">Buy Now</div>
-</div>
-<!-- Javascript Files
-================================================== -->
-@include("user.layouts.scripts")
-
-</body>
-
-</html>
+@endsection
